c0cd6094f329e3863d851684e1f3903c2bb41315
Sebastiao Pereira <thiagoedanielamartins@gmail.com>
2023-11-10 20:16:40 -0300

    desafios trilha dotnet dio

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,61 @@
+using DesafioProjetoHospedagem.Models;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if(Suite is null){ throw new Exception("Nenhuma suíte foi definida!");}
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Capacidade da suíte não atende ao número de hospedes informados!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes) 
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0;
+
+             valor = DiasReservados * Suite.ValorDiaria;
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados>=10)
+            {
+                valor = valor  - (valor * (decimal)0.1);
+            }
+
+            return valor;
+        }
+    }
+}
